6b21516e8e8ecdfc0fd98ba4a9cd5fcfbb723349
王亮 <1392830517@qq.com>
2021-04-20 21:51:02 +0800

    update: router自动注册

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,6 @@ const routes = [
     path: '/',
     redirect: '/login'
   }
-  // {
-  //   path: '/login',
-  //   name: 'Login',
-  //   component: () => import('@/views/login')
-  // },
-  // {
-  //   path: '/home',
-  //   name: 'Home',
-  //   component: () => import('@/views/home')
-  // },
-  // {
-  //   path: '/search',
-  //   name: 'Search',
-  //   component: () => import('@/views/search')
-  // }
 ]
 
 const routeContext = require.context('@/views', true, /index.vue/)
@@ -31,7 +16,6 @@ routeContext.keys().forEach(routePath => {
     const route = {
       path,
       component: () => routeContext(routePath),
-      // component: () => import(`@/views${path}`),
       name: capitalize(path.replace('/', ''))
     }
     routes.push(route)
