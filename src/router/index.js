bc33d76ea5e2e7918eb2af0b839099c7f10e64dc
王亮 <1392830517@qq.com>
2021-04-20 21:47:13 +0800

    update: router自动注册

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// import { capitalize } from 'lodash'
 
 const routes = [
   {
@@ -14,8 +15,27 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: () => import('@/views/home')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import('@/views/search')
   }
 ]
+
+// const routeContext = require.context('@/views', true, /index.vue/)
+
+// routeContext.keys().forEach(routePath => {
+//   const path = routePath.match(/\/.*\//)  ? routePath.match(/\/.*\//)[0].slice(0, -1) : ''
+//   if (path) {
+//     const route = {
+//       path,
+//       component: () => import(`@/views${path}`),
+//       name: capitalize(path.replace('/', ''))
+//     }
+//     routes.push(route)
+//   }
+// })
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
