91bc1671cb8e5ac425e726e59d6b5e0107424f78
王亮 <1392830517@qq.com>
2021-04-26 07:53:16 +0800

    update: 书写Image组件样式

--- src/components/Image/index.vue
+++ src/components/Image/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="image">
-    image
+    <h3>客户LOGO</h3>
+    <div class="image-wrapper">
+      <van-image></van-image>
+      <p class="tips">提示：将作为客户商城的LOGO</p>
+    </div>
   </div>
 </template>
 
@@ -20,5 +24,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.image{
+  margin: 24px;
+  background: #FFFFFF;
+  border-radius: 16px;
+  h3 {
+    padding: 24px 30px 20px;
+    font-size: 28px;
+    font-family: PingFang-SC-Medium, PingFang-SC;
+    font-weight: 500;
+    color: #292929;
+    line-height: 40px;
+  }
+  .image-wrapper {
+    text-align: center;
+    overflow: hidden;
+  }
+  ::v-deep(.van-image){
+    width: 514px;
+    height: 268px;
+    background: #F4F5F6;
+    border-radius: 12px;
+    border: 2px solid #E6E6E6;
+  }
+  .tips {
+    margin: 16px 0 28px;
+    font-size: 24px;
+    font-family: PingFang-SC-Medium, PingFang-SC;
+    font-weight: 500;
+    color: #999999;
+    line-height: 36px;
+  }
+}
 </style>
