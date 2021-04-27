ec8c5fdd35503e6a63cfd940150e32c9bf0a7ef9
王亮 <1392830517@qq.com>
2021-04-27 15:24:26 +0800

    update: 新增Checkbox组件

---
+++ src/components/Checkbox/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <van-checkbox v-bind="$attrs"></van-checkbox>
+</template>
+
+<script>
+export default {
+  name: 'Checkbox',
+  components: {
+  },
+  props: {
+  },
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/addCustomer/index.vue
+++ src/views/addCustomer/index.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="add-customer">
-    <y-steps></y-steps>
-    <section class="section">
-      <y-field label="所属组织" required placeholder="请选择所属组织" is-link></y-field>
-      <y-field label="父级一/父级二" model-value="芭芭拉/巴布洛塔罗夫斯基" readonly></y-field>
-      <y-field label="客户名称" required placeholder="请输入客户名称"></y-field>
-      <y-field label="客户地址" required placeholder="请输入客户名称" is-link></y-field>
-      <y-field label="详细地址" required placeholder="请输入具体地址"></y-field>
-    </section>
-    <section class="section">
-      <y-field label="联系人" required placeholder="请输入联系人姓名"></y-field>
-      <y-field label="联系电话" required placeholder="请输入客户联系电话" type="tel"></y-field>
-      <y-field label="固定电话" placeholder="请输入客户固定电话" type="number"></y-field>
-      <y-field label="邮箱" placeholder="请输入客户邮箱"></y-field>
-      <y-field label="分析账户" required placeholder="请选择分析账户" is-link></y-field>
-      <y-field label="税号" required placeholder="请输入税号"></y-field>
-      <y-field label="是否为实际节点" required>
-        <template #right-icon>
-          <y-switch v-model="form.switch"></y-switch>
-        </template>
-      </y-field>
-    </section>
-    <y-tips>提示：非实际节点客户仅需配置基础信息</y-tips>
+    <y-steps v-if="step" :step="step"></y-steps>
+    <div v-if="step <= 1" class="section-step">
+      <section class="section">
+        <y-field label="所属组织" required placeholder="请选择所属组织" is-link></y-field>
+        <y-field label="父级一/父级二" model-value="芭芭拉/巴布洛塔罗夫斯基" readonly></y-field>
+        <y-field label="客户名称" required placeholder="请输入客户名称"></y-field>
+        <y-field label="客户地址" required placeholder="请输入客户名称" is-link></y-field>
+        <y-field label="详细地址" required placeholder="请输入具体地址"></y-field>
+      </section>
+      <section class="section">
+        <y-field label="联系人" required placeholder="请输入联系人姓名"></y-field>
+        <y-field label="联系电话" required placeholder="请输入客户联系电话" type="tel"></y-field>
+        <y-field label="固定电话" placeholder="请输入客户固定电话" type="number"></y-field>
+        <y-field label="邮箱" placeholder="请输入客户邮箱"></y-field>
+        <y-field label="分析账户" required placeholder="请选择分析账户" is-link></y-field>
+        <y-field label="税号" required placeholder="请输入税号"></y-field>
+        <y-field label="是否为实际节点" required>
+          <template #right-icon>
+            <y-switch v-model="form.switch"></y-switch>
+          </template>
+        </y-field>
+      </section>
+      <y-tips>提示：非实际节点客户仅需配置基础信息</y-tips>
+    </div>
+    <div v-if="step === 2" class="section-step">
+      <section class="section">
+        <y-field label="收货地址" required placeholder="请选择省/市/区" is-link readonly></y-field>
+        <y-field label="详细地址" required placeholder="请输入收货地址"></y-field>
+      </section>
+    </div>
     <y-button>确认创建</y-button>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 export default {
   name: 'AddCustomer',
   components: {
@@ -38,8 +46,10 @@ export default {
     const form = reactive({
       switch: true
     })
+    const step = ref(2) // 0 : 初始状态, 1: 表示第一步, 2: 表示第二步
     return {
-      form
+      form,
+      step
     }
   }
 }
